e9d104735bb8edd68928fe5dfcfad1e441713780
gitccsand <gozilla@163.com>
2015-11-04 11:37:17 +0800

    20151104

--- www/handlers.py
+++ www/handlers.py
@@ -4,3 +4,13 @@
 __author__ = 'Michael Liao'
 
 ' url handlers '
+
+import coroweb
+import models
+@coroweb.get('/')
+def index():
+    users = (yield from models.User.findAll())
+    return {
+        '__template__' : 'test.html',
+        'users' : users
+        }
